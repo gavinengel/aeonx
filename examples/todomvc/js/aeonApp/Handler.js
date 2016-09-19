b9d7ee03cbe7615d59ab0cbcc658683572d0156b
Gavin Engel <gavin@engel.com>
2016-09-18 21:25:07 -0400

    .

--- examples/todomvc/js/aeonApp/Handler.js
+++ examples/todomvc/js/aeonApp/Handler.js
@@ -1,35 +1,12 @@
 
 
 /**
- *
- */
-var $testr = function(e) {
-    alert('testr!');
-  if (e.target) {
-
-    console.log({'testerE':e});
-  }
-}
-
-
-/**
  */
 var $addTodo = function(e, msg, id, completed, skipStorage) {
   if (e) var msg = e.target[0].value;
 
   if (msg) {
     var newTag = document.createElement('li'); 
-    // before move .destory and .toggle to .aeon:
-    //var html = '<div class="view"><input class="toggle" type="checkbox" onclick="return window.Handler.toggleTodo(event) || false;"><label>'+msg+'</label>' +
-    //  '<button class="destroy" onclick="return window.Handler.delTodo(event) || false;"></button> </div> <input class="edit" value="asdf">'
-
-    // after move .destroy:
-    //var html = '<div class="view"><input class="toggle" type="checkbox" onclick="return window.Handler.toggleTodo(event) || false;"><label>'+msg+'</label>' +
-    //  '<button someattr="" class="destroy"></button> </div> <input class="edit" value="asdf">'
-
-    // after move .toggle:
-    //var html = '<div class="view"><input class="toggle" type="checkbox"><label>'+msg+'</label>' +
-    //  '<button class="destroy"></button> </div> <input class="edit" value="asdf">'
 
     // convert label to input:
     var checked = (completed)? 'checked="checked"' : '';
@@ -134,75 +111,8 @@ var $clearCompleted = function(e) {
     elm.parentNode.removeChild(elm); // TODO caniuse: remove();
   }
 }
-/*
-
-before:
-
-        <ul class="todo-list"></ul>
-
-/*
-
-after:
-
-    <ul class="todo-list"><li>
-      <div class="view">
-        <input class="toggle" type="checkbox">
-        <label>asdf</label>
-        <button class="destroy"></button>
-      </div>
-      <input class="edit" value="asdf">
-    </li>
-    </ul>
-
-
-/*
-var $destroy = function(e) {
-  // get the li-id
-  var id = $getNearestLiId(e.target)
-  
-  // remove li
-  //console.log(window)
-  //console.log(app)
-  //var $Controller = app.Controller()
-  //Model.remove(id)
-  todo.controller.removeItem(id)
-
-}
-
-
-
-var $getNearestLiId = function(node) {
-  var parent = $getParentByTagName(node, 'li')
-  //console.log({parent: parent})
-  return parent.getAttribute('data-id')
-}
-
-/**
- * Get parent node for given tagname
- * @param  {Object} node    DOM node
- * @param  {String} tagname HTML tagName
- * @return {Object}         Parent node
- */
-/*function $getParentByTagName(node, tagname) {
-  var parent;
-  if (node === null || tagname === '') return;
-  parent  = node.parentNode;
-  tagname = tagname.toUpperCase();
-
-  while (parent.tagName !== "HTML") {
-    if (parent.tagName === tagname) {
-      return parent;
-    }
-    parent = parent.parentNode;
-  }
-
-  return parent;
-}
-*/
 
 Handler = {
-    //destroy: $destroy,
-    testr: $testr,
     clearCompleted: $clearCompleted,
     addTodo: $addTodo,
     delTodo: $delTodo,
